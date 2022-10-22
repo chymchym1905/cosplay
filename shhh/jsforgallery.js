0ec81d2076480af14948b77533133819398e954b
chymchym1905 <kien.dauoofman@hcmut.edu.vn>
2022-10-22 21:14:09 +0700

    git pull

--- shhh/jsforgallery.js
+++ shhh/jsforgallery.js
@@ -4,7 +4,26 @@ let picCount = document.querySelectorAll('.column img').length;
 
 window.onresize = window.onload = function(){
     resize(document.querySelector('.pop-up img'));
+    //rearrange(document.querySelectorAll('.thirty-percent-extra-row'));
 }
+
+
+
+// function rearrange(col_with_3_rol){
+//     winsize = getWinDim();
+//     if(winsize.x<800 && winsize.x>600){
+//         for(y = 0; y<col_with_3_rol.length;y++){
+//             col_with_3_rol[y].classList.add('grid-disp');
+//             console.log(col_with_3_rol[y].classList);
+//         }
+//     }
+//     if(winsize.x>=800){
+//         for(x = 0; x<col_with_3_rol.length;x++){
+//             col_with_3_rol[x].classList.remove('grid-disp');
+//         }
+
+//     }
+// }
 
 function resize(img)
 {
@@ -27,7 +46,8 @@ function getWinDim(){
     }
 }
 
-document.querySelectorAll('.column img, .biggestcol img').forEach( img=> {
+
+document.querySelectorAll('.column img, .biggestcol img, .thirty-percent-extra-row img, .row-with-2-col img').forEach( img=> {
     img.onclick = () =>{
         document.querySelector('.pop-up img').src = img.getAttribute('src');
         resize(document.querySelector('.pop-up img'));
@@ -37,7 +57,7 @@ document.querySelectorAll('.column img, .biggestcol img').forEach( img=> {
     }
 });
 
-document.querySelectorAll('.column img').forEach( gridimd =>{
+document.querySelectorAll('.column img, .biggestcol img').forEach( gridimd =>{
     gridimd.onload = () =>{
         console.log(gridimd.naturalWidth + " and " + gridimd.naturalHeight + " "+ gridimd.getAttribute('id'));
         if(gridimd.naturalWidth > gridimd.naturalHeight){
